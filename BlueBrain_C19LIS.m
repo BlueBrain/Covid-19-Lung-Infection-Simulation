bb4f1ae5093640ed6d31336c7dd8edd4f6f8f18a
jaycoggan <82528048+jaycoggan@users.noreply.github.com>
2021-04-14 23:51:03 +0200

    Add files via upload

--- BlueBrain_C19LIS.m
+++ BlueBrain_C19LIS.m
@@ -1,3 +1,4 @@
+%{
 Covid-19 Lung Infection Simulator (C19-LIS)
 
 MIT License
@@ -22,6 +23,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 
+%}
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
